150b2ac588b3955a3ce038ff20274f1c21e0ce07
Duc Trung <trung.lyduc18@gmail.com>
2024-10-12 18:51:07 +0700

    CHECKPOINT: login api

--- recommend_system_project/src/services/userService.py
+++ recommend_system_project/src/services/userService.py
@@ -16,75 +16,67 @@ async def login(request: OAuth2EmailPasswordRequestForm):
         status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized."
     )
 
-    #
-
-    # Query username (unique) in database
+    # Query email in database
     user = neo4j_models.User.match(pp=request.email)
-    print(user)
-    print(type(user))
 
-    # if (
-    #     request.username != MOCK_DATABASE["user"]["email"]
-    #     or request.password != MOCK_DATABASE["user"]["password"]
-    # ):
-    #     raise unauthorized_exception
+    if not user:
+        raise unauthorized_exception
 
-    # # Create payload to include in JWT token
-    # user_info = {
-    #     "id": MOCK_DATABASE["user"]["id"],
-    #     "email": MOCK_DATABASE["user"]["email"],
-    # }
+    # Create payload to include in JWT token
+    user_info = {
+        "email": user.email,
+    }
 
-    # # Create access token and refresh token
-    # access_token = jwtProvider.generate_token(
-    #     user_info,
-    #     environment.ACCESS_TOKEN_SECRET_KEY,
-    #     configuration.ACCESS_TOKEN_LIFETIME,
-    # )
+    # Create access token and refresh token
+    access_token = jwtProvider.generate_token(
+        user_info,
+        environment.ACCESS_TOKEN_SECRET_KEY,
+        configuration.ACCESS_TOKEN_LIFETIME,
+    )
 
-    # refresh_token = jwtProvider.generate_token(
-    #     user_info,
-    #     environment.REFRESH_TOKEN_SECRET_KEY,
-    #     configuration.REFRESH_TOKEN_LIFETIME,
-    # )
+    refresh_token = jwtProvider.generate_token(
+        user_info,
+        environment.REFRESH_TOKEN_SECRET_KEY,
+        configuration.REFRESH_TOKEN_LIFETIME,
+    )
 
-    # # USE ONE OF THESE: Local storage OR HTTP only cookie
+    # USE ONE OF THESE: Local storage OR HTTP only cookie
 
-    # response = None
+    response = None
 
-    # # Local storage: Return user info and tokens
-    # if not TOKEN_LOCATION or TOKEN_LOCATION == enums.TokenLocation.HEADER:
-    #     response = JSONResponse(
-    #         content={
-    #             **user_info,
-    #             "access_token": access_token,
-    #             "refresh_token": refresh_token,
-    #         }
-    #     )
+    # Local storage: Return user info and tokens
+    if not TOKEN_LOCATION or TOKEN_LOCATION == enums.TokenLocation.HEADER:
+        response = JSONResponse(
+            content={
+                **user_info,
+                "access_token": access_token,
+                "refresh_token": refresh_token,
+            }
+        )
 
-    # # HTTP only cookie for browser
-    # # max_age is lifetime of cookie, could be equal to refresh_token lifetime
-    # elif TOKEN_LOCATION == enums.TokenLocation.COOKIES:
-    #     response = JSONResponse(content={**user_info})
-    #     response.set_cookie(
-    #         key="access_token",
-    #         value=access_token,
-    #         httponly=True,
-    #         secure=True,
-    #         samesite=None,
-    #         max_age=configuration.COOKIES_LIFETIME.total_seconds() * 1000,
-    #     )
+    # HTTP only cookie for browser
+    # max_age is lifetime of cookie, could be equal to refresh_token lifetime
+    elif TOKEN_LOCATION == enums.TokenLocation.COOKIES:
+        response = JSONResponse(content={**user_info})
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite=None,
+            max_age=configuration.COOKIES_LIFETIME.total_seconds() * 1000,
+        )
 
-    #     response.set_cookie(
-    #         key="refresh_token",
-    #         value=refresh_token,
-    #         httponly=True,
-    #         secure=True,
-    #         samesite=None,
-    #         max_age=configuration.COOKIES_LIFETIME.total_seconds() * 1000,
-    #     )
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,
+            secure=True,
+            samesite=None,
+            max_age=configuration.COOKIES_LIFETIME.total_seconds() * 1000,
+        )
 
-    # return response
+    return response
 
 
 async def logout(request):
